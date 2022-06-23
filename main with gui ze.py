c13867339dc72e9468125bc0050da550400ac603
Urbinator <simon.urban@alumni.esade.edu>
2022-06-23 14:45:45 +0200

    Cleaned up code, inserted MIT License, added docstrings

--- main with gui ze.py
+++ main with gui ze.py
@@ -1,3 +1,26 @@
+# MIT License
+# 
+# Copyright (c) 2022 Simon Urban, Robert Bernasconi, Casper Kirch, Jose Marques, Akshit XXX
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import numpy as np
 import pygame
 from time import sleep
@@ -19,15 +42,47 @@ size = (width, height)
 #GLOBAL VARIABLES END
 
 class Connect4:
+    '''Main Connect 4 class with all functionalities needed
+    Attributes
+    -----------
+    Player: int
+        Which player is currently playing (0 or 1)
+    column: int
+        Int from 0 to 5 representing the column where to place the coin
+    
+    Methods
+    ----------
+    create_board()
+        creates empty Connect 4 matrix
+    draw_board()
+        visualizes empty board
+    print_current_board()
+        prints current status of the board
+    __check_move_allowed()
+        checks if the desired move is allowed/ possible
+    place_coin()
+        places the coin in the desired column, after the desired move is checked regarding feasibility
+    check_win()
+        checks the winning conditions
+    event_handler()
+        inputs users interactions into the place_coin() function for each player and checks for winning condition via the check_win() function
+    draw_coin()
+        creates a new coin at the beginning of each player's turn
+    gameloop()
+        loops through event_handler() until winning condition is met
+
+    '''
     def __init__(self):
         '''
-        Initialize:
+        Initialization function
             pygame: with a title "CONEECT 4"
             screen: A UI screen for the users to interact upon
             __container: a hidden parameter that contains the up to date values of the game.
-            turn: sets turn to 0
+            turn: it's an int (0)
             myfont: defines UI font and font size
             game_over: initialy set to False
+            
+            return: None
         '''
         pygame.init()
         pygame.display.set_caption('CONNECT 4')
@@ -64,32 +119,33 @@ class Connect4:
                         int(c * SQUARESIZE + SQUARESIZE / 2), int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2)),
                                        RADIUS)
         pygame.display.update()
-
-    def print_board(self):
+ 
+    #FIXME: is function print_current_board() still needed, if pygame visualizes and updates baord via draw_board() ?
+    def print_current_board(self):
         '''
         This function prints the current state of the board
         '''
         print(self.__container)
         # print('  0 1  2  3  4  5  6')
 
-    def __is_not_full(self, column: int) -> bool:
+    def __check_move_allowed(self, column: int) -> bool:
         '''
-        This function checks if the move is allowed, i.e. the column is not full
+        This hidden function checks if the move is allowed, i.e. the column is not full
             column: an int from 0 to 5 representing the column where to check if is not full
             returns: True if the move is allowed
         '''
         if self.__container[0, column] == 0:
             return True
 
-    def position_coin(self, column: int, player: int) -> bool:
+    def place_coin(self, column: int, player: int) -> bool:
         '''
-        This function check if a move is allowed and place the coin in the selected column.
-        In the selected column it check from the bottom which is the first empty place to place it.
+        This function checks if a move is allowed and place the coin in the selected column.
+        In the selected column it checks from the bottom which is the first empty place to place it.
             column: an int from 0 to 5 representing the column where to place the coin
             player: an int (0 or 1) representing the current player making the move
             returns: True if the move was allowed and the board is updated, False if the move was not allowed
         '''
-        res = self.__is_not_full(column)
+        res = self.__check_move_allowed(column)
         if res:
             for row in reversed(range(len(self.__container[:, column]))):
                 if self.__container[row, column] == 0:
@@ -150,12 +206,13 @@ class Connect4:
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pygame.draw.rect(self.screen, BLACK, (0, 0, width, SQUARESIZE))
-                # Ask for Player 1 Input
+                
                 if self.turn == 0:
+                # Ask for Player 1 Input
                     posx = event.pos[0]
                     col = int(math.floor(posx / SQUARESIZE))
 
-                    if self.position_coin(col, 1):
+                    if self.place_coin(col, 1):
 
                         if self.check_win(1) == 1:
                             label = self.myfont.render("Player 1 wins!!", 1, RED)
@@ -169,7 +226,7 @@ class Connect4:
                 # Ask for Player 2 Input
                     posx = event.pos[0]
                     col = int(math.floor(posx / SQUARESIZE))
-                    if self.position_coin(col, 2):
+                    if self.place_coin(col, 2):
                         if self.check_win(2) == 2:
                             label = self.myfont.render("Player 2 wins!!", 1, YELLOW)
                             self.screen.blit(label, (110,0))
@@ -178,7 +235,7 @@ class Connect4:
                             label = self.myfont.render("It's a Draw!!", 1, RED)
                             self.screen.blit(label, (110,0))
                             self.game_over = True
-                self.print_board()
+                self.print_current_board()
 
                 self.turn += 1
                 self.turn = self.turn % 2
